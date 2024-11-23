d09c89c51ab805ee472916ba25583553141a7e0f
SerhiiNikif <nikiforenko.serhii@gmail.com>
2024-11-23 17:00:16 +0200

    env

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Додати порт із змінної оточення
   const port = process.env.PORT || 3000;
+  const host = process.env.HOST || '127.0.0.1';
+
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Application is running on: http://${host}:${port}`);
 }
 bootstrap();
