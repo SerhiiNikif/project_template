82101b991f028f0cca732f69818e280397f88d42
SerhiiNikif <nikiforenko.serhii@gmail.com>
2024-11-23 17:19:57 +0200

    test2

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT || 3000;
-  const host = process.env.HOST || '127.0.0.1';
 
   await app.listen(port);
-  console.log(`Application is running on: http://${host}:${port}`);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
